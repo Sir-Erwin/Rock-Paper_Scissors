beb8224ba90390a48e271cf947c55066a161c0dd
Erwin2431 <k1829435@students.katyisd.org>
2020-02-25 07:46:05 -0600

    Done.

---
+++ Rock-Paper_Scissors/src/Main.java
@@ -0,0 +1,16 @@
+
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner a = new Scanner(System.in);
+
+        System.out.println("Hi, I'm Thunderhead, what is your name?");
+        String name = a.nextLine();
+
+        System.out.println("\nHello " + name + ", Let the games begin!");
+        Game game = new Game(name);
+
+    }
+}
